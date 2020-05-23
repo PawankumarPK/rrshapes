022c6b68779b3c3add7d6213b65dd50ed3937728
PawankumarPK <pk9910765616@gmail.com>
2020-05-23 22:39:44 +0530

    Fix bug

--- app/src/main/java/ai/handypawan/shapes/MainActivity.kt
+++ app/src/main/java/ai/handypawan/shapes/MainActivity.kt
@@ -1,12 +1,20 @@
 package ai.handypawan.shapes
 
+import ai.handypawan.bitmapimage.Shapes
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val shapes = Shapes()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*val bitmap = BitmapFactory.decodeResource(resources,R.drawable.pawan)
+        mImageview.setImageBitmap( shapes.getCroppedBitmap(bitmap))*/
     }
 }
---
+++ rrshapes/src/main/java/ai/handypawan/bitmapimage/Shapes.java
@@ -0,0 +1,33 @@
+package ai.handypawan.bitmapimage;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+
+/**
+ * Created by pawan on 23,May,2020
+ */
+public class Shapes {
+
+     public Bitmap getCroppedBitmap(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawCircle(bitmap.getWidth() / 2f, bitmap.getHeight() / 2f,
+                bitmap.getWidth() / 2f, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+}
