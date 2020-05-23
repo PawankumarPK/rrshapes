e6c1e0d92df5f3b407b145a125ff2fe5d5a8a032
PawankumarPK <pk9910765616@gmail.com>
2020-05-23 20:21:33 +0530

    Circle bitmapimage cutter

---
+++ app/src/main/java/ai/handypawan/shapes/MainActivity.kt
@@ -0,0 +1,12 @@
+package ai.handypawan.shapes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ rrshapes/src/main/java/ai/handypawan/bitmapimage/Shapes.kt
@@ -0,0 +1,32 @@
+package ai.handypawan.bitmapimage
+
+import android.graphics.*
+
+/**
+ * Created by pawan on 23,May,2020
+ */
+class Shapes {
+
+    fun circleCroppedBitmap(bitmap: Bitmap): Bitmap? {
+        val output = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(output)
+        val color = -0xbdbdbe
+        val paint = Paint()
+        val rect = Rect(0, 0, bitmap.width, bitmap.height)
+        paint.isAntiAlias = true
+        canvas.drawARGB(0, 0, 0, 0)
+        paint.color = color
+        // canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        canvas.drawCircle(
+            (bitmap.width / 2).toFloat(),
+            (bitmap.height / 2).toFloat(),
+            (bitmap.width / 2).toFloat(),
+            paint
+        )
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        canvas.drawBitmap(bitmap, rect, rect, paint)
+        //Bitmap _bmp = Bitmap.createScaledBitmap(output, 60, 60, false);
+        //return _bmp;
+        return output
+    }
+}
